c9380f4605a1059e2d57443780f9760ae2dec06c
harry <harrypalmer889@gmail.com>
2020-11-25 17:34:42 +0000

    more work on refactoring

---
+++ scripts/credits.js
@@ -0,0 +1,13 @@
+function formSubmission() {
+    var form = document.querySelector('.formContainer');
+    var name = document.querySelector('.name').value;
+    var email = document.querySelector('.email').value;
+    var message = document.querySelector('.message').value;
+    var confirm = document.querySelector('.formConfirm')
+
+    if (name, email, message) {
+        console.log('all criterial filled')
+        form.classList.add('hidden')
+        confirm.classList.remove('hidden')
+    }
+}
